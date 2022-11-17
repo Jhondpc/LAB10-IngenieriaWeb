33b5f05e01c971ea67df7228acdeef2ecfe2aabd
Jhon Pardo <a20196137@pucp.edu.pe>
2022-11-17 17:49:37 -0500

    metodos ok

---
+++ Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/Dtos/DtoCantidadContratos.java
@@ -0,0 +1,23 @@
+package com.example.lab10_20182895_20196137.Dtos;
+
+public class DtoCantidadContratos {
+
+    private String estadoContrato;
+    private int numeroContratos;
+
+    public String getEstadoContrato() {
+        return estadoContrato;
+    }
+
+    public void setEstadoContrato(String estadoContrato) {
+        this.estadoContrato = estadoContrato;
+    }
+
+    public int getNumeroContratos() {
+        return numeroContratos;
+    }
+
+    public void setNumeroContratos(int numeroContratos) {
+        this.numeroContratos = numeroContratos;
+    }
+}
---
+++ Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/Dtos/DtoMaxExpectedLoss.java
@@ -0,0 +1,22 @@
+package com.example.lab10_20182895_20196137.Dtos;
+
+public class DtoMaxExpectedLoss {
+    private String nombreCliente;
+    private float maxExpectedLoss;
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public float getMaxExpectedLoss() {
+        return maxExpectedLoss;
+    }
+
+    public void setMaxExpectedLoss(float maxExpectedLoss) {
+        this.maxExpectedLoss = maxExpectedLoss;
+    }
+}
