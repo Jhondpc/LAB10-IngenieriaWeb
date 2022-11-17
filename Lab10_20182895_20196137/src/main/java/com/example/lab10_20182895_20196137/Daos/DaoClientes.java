806e205637f3bf9a09ed888f8fbf75a07e7a46ff
alexiajg22 <a20192270@pucp.edu.pe>
2022-11-16 20:58:14 -0500

    base

---
+++ Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/Daos/DaoClientes.java
@@ -0,0 +1,43 @@
+package com.example.lab10_20182895_20196137.Daos;
+
+import com.example.lab10_20182895_20196137.beans.Clientes;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DaoClientes extends DaoBase {
+
+    public ArrayList<Clientes> listarClientes(){
+
+        ArrayList<Clientes> listaClientes = new ArrayList<>();
+        try{
+            Connection connection = getConnection();
+            String sql = "select concat(c.g4093_name, ' ' , coalesce( c.g4093_last_name ,'')) as 'Cliente',\n" +
+                    "\t\tc.g4093_age as 'Edad',\n" +
+                    "\t\tcase when c.g4093_type='N' then 'Normal' when c.g4093_type='J' then 'Juridica' end as 'Tipo de Cliente',\n" +
+                    "        c.g4093_documentType as 'Tipo de Documento',\n" +
+                    "        c.g4093_nro_id as 'Numero de Documento',\n" +
+                    "        count(g4093_nro_id) as 'Cantidad de Contratos'\n" +
+                    "from jm_client_bii c inner join jm_cotr_bis p on (c.g4093_nro_id = p.client_nro_id)\n" +
+                    "group by g4093_nro_id;";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                Clientes clientes = new Clientes();
+                clientes.setNombreCliente(rs.getString(1));
+                clientes.setEdad(rs.getString((2)));
+                clientes.setTipoCliente(rs.getString(3));
+                clientes.setTipoDocumento(rs.getString(4));
+                clientes.setNumeroDocumento(rs.getString(5));
+                clientes.setCantidadDeContratos(rs.getInt(6));
+                listaClientes.add(clientes);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listaClientes;
+
+    }
+}
---
+++ Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/beans/Clientes.java
@@ -0,0 +1,58 @@
+package com.example.lab10_20182895_20196137.beans;
+
+public class Clientes {
+    private String nombreCliente;
+    private String edad;
+    private String tipoCliente;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private int cantidadDeContratos;
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getCantidadDeContratos() {
+        return cantidadDeContratos;
+    }
+
+    public void setCantidadDeContratos(int cantidadDeContratos) {
+        this.cantidadDeContratos = cantidadDeContratos;
+    }
+}
