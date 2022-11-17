610c64409b4205aa9e47b4017897220b0e4a61cf
Jhon Pardo <a20196137@pucp.edu.pe>
2022-11-17 00:54:49 -0500

    DaoAdmin

--- Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/Daos/DaoAdmin.java
+++ Lab10_20182895_20196137/src/main/java/com/example/lab10_20182895_20196137/Daos/DaoAdmin.java
@@ -1,6 +1,23 @@
 package com.example.lab10_20182895_20196137.Daos;
 
-public class DaoAdmin {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DaoAdmin extends DaoBase{
+    public void createCredentialCliente(String numeroDocumento, String password) {
+
+        String sql = "INSERT INTO `bi_corp_business`.`credentials` (`nro_documento`, `password`, `hashedPassword`, `tipoUsuario`) VALUES (?, ?, SHA2(?,256), '2');";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, numeroDocumento);
+            pstmt.setString(2, password);
+            pstmt.setString(3, password);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
